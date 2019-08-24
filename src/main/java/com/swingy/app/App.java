e354a0c1d6b929eccca54adb7dcbbe6a4cc66b6b
rogerndaba <rogerndaba@gmail.com>
2019-08-24 15:40:56 +0200

    added welcome message

--- src/main/java/com/swingy/app/App.java
+++ src/main/java/com/swingy/app/App.java
@@ -5,30 +5,73 @@ package com.swingy.app;
  *
  */
 
- import java.awt.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
 
- import javax.swing.JFrame;
- import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import com.swingy.views.Colors;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        JFrame frame = new JFrame("Roger");
+public class App {
 
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
+        if (args.length < 1) {
+            String text = Colors._CYAN
+                    + "\nWelcome to the hero war game.\nStarting mode can be chosen\nbetween Console (Default) and GUI. You can choose to\ncreate a new hero or choose from previously created heros.\nIf there are no previously created\nheros, you'll be required to create a new hero.\n\n"
+                    + Colors._RESET;
+            int i;
+            for (i = 0; i < text.length(); i++) {
+                System.out.printf("%c", text.charAt(i));
+                try {
+                    if (text.charAt(i) == '.')
+                        Thread.sleep(500);
+                    else
+                        Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            System.out.print("Would you like to create a new hero? (y|n) : ");
+            String opt = scn.nextLine().trim();
+            System.out.println(opt);
+        } else {
+            for (String i : args) {
+                System.out.println(i);
+            }
+        }
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // JFrame frame = new JFrame("Roger");
 
-        JLabel label = new JLabel("I'm here");
-        label.setPreferredSize(new Dimension(300, 100));
-        frame.getContentPane().add(label, BorderLayout.CENTER);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setPreferredSize(new Dimension(900, 600));
+        // frame.getContentPane().setBackground(Color.BLACK);
 
-        frame.setLocationRelativeTo(null);
-        frame.pack();
-        frame.setVisible(true);
+        // JLabel label = new JLabel("I'm here");
+        // label.setPreferredSize(new Dimension(300, 100));
+        // frame.setFocusable(true);
+        // frame.getContentPane().add(label, BorderLayout.CENTER);
+
+        // frame.addKeyListener(new KeyListener() {
+        // public void keyTyped(KeyEvent e) {
+        // }
+
+        // public void keyPressed(KeyEvent e) {
+        // System.out.println("key : " + e.getKeyCode());
+        // }
+
+        // public void keyReleased(KeyEvent e) {
+        // }
+        // });
+        // frame.pack();
+        // frame.setVisible(true);
+        // while (true) {
+        // frame.repaint();
+        // // System.out.println("Hello World!");
+        // }
         // jFrame.setBackground(new Color(255));
-
-        System.out.println( "Hello World!" );
+        scn.close();
     }
 }
---
+++ src/main/java/com/swingy/views/Colors.java
@@ -0,0 +1,16 @@
+package com.swingy.views;
+
+/**
+ * Colors
+ */
+public class Colors {
+	public static final String _RESET = "\u001B[0m";
+	public static final String _BLACK = "\u001B[1;30m";
+	public static final String _RED = "\u001B[1;31m";
+	public static final String _GREEN = "\u001B[1;32m";
+	public static final String _YELLOW = "\u001B[1;33m";
+	public static final String _BLUE = "\u001B[1;34m";
+	public static final String _PURPLE = "\u001B[1;35m";
+	public static final String _CYAN = "\u001B[1;36m";
+	public static final String _WHITE = "\u001B[1;37m";
+}
