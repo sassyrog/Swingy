a9c82e2f0477480bc67b0a97ee182ccc649ba878
Rogerndaba <rogerndaba@gmail.com>
2019-08-30 11:40:33 +0200

    added getters and setters on Position

--- src/main/java/com/swingy/models/Position.java
+++ src/main/java/com/swingy/models/Position.java
@@ -4,9 +4,25 @@ package com.swingy.models;
  * Position
  */
 public class Position {
-	private int x;
-	private int y;
+	private int _x;
+	private int _y;
 
 	public Position(int level) {
+	}
+
+	public void setX(int x) {
+		this._x = x;
+	}
+
+	public void setY(int y) {
+		this._y = y;
+	}
+
+	public int getX() {
+		return this._x;
+	}
+
+	public int getY() {
+		return this._y;
 	}
 }
